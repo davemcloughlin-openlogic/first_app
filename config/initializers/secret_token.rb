f8bad077fa896c05ec9549b5d2ae55fcb0a4eda9
Dave McLoughlin <dave.mcloughlin@openlogic.com>
2014-04-29 16:30:23 -0600

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '264ff26644bd83aced1a6d87e346de7907cb51fab34916a7d5b88de538b2e0adece8b6e443c1e4e3111d3c5e2bc8cdedb63ba974bac8db377744bd3675401193'
